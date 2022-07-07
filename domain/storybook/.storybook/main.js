74fb2bc65628ccee676d5dc568c9f66437de9668
Your Name <you@example.com>
2022-07-06 22:41:18 -0400

    feat: progress with storybook synchronization

--- domain/storybook/.storybook/main.js
+++ domain/storybook/.storybook/main.js
@@ -1,12 +1,23 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../../apps/**/src/components/**/*.stories.mdx",
+    "../../../apps/**/src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../../features/**/src/components/**/*.stories.mdx",
+    "../../../features/**/src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../../packages/**/components/**/stories/*.stories.mdx",
+    "../../../packages/**/components/**/stories/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    { name: "@storybook/addon-essentials", options: { actions: false } },
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  features: {
+    interactionsDebugger: true,
+    features: { buildStoriesJson: true },
+  },
+};
--- domain/storybook/stories/Button.stories.jsx
+++ domain/storybook/stories/Button.stories.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { within, userEvent } from "@storybook/testing-library";
 
-import { Button } from './Button';
+import { expect } from "@storybook/jest";
+import { Button } from "./Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
 };
 
@@ -19,22 +22,31 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: "Button",
 };
+Primary.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root element
+  const canvas = within(canvasElement);
+  const button = canvas.getByRole("button");
+  // See https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
+  await userEvent.click(button);
 
+  // 👇 Assert DOM structure
+  await expect(canvas.getByText("Button")).toBeInTheDocument();
+};
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: "large",
+  label: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: "small",
+  label: "Button",
 };
